4fb6a406eea5cfcb3295c468861f616b2e2c3e62
shahzeel <shazeelarain786@gmail.com>
2025-04-29 19:52:34 +0500

    added cors

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,12 @@ import express from "express";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import productRoute from "./routes/product.route.js";
+import cors from "cors";
+
 
 const app = express();
 const port = process.env.PORT || 3000; // Use the port from environment variables or default to 3000
-
+app.use(cors()); 
 app.use(express.json()); // Middleware to parse JSON request body
 app.use("/api/products", productRoute); // Mount the product route on /products
 
