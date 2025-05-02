7d55790a430d51d9b41d7eea569e0a70ce3c2c09
shahzeel <shazeelarain786@gmail.com>
2025-05-02 18:22:44 +0500

    final code before deployement

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ import express from "express";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import productRoute from "./routes/product.route.js";
+import cors from "cors"; 
 
 
 dotenv.config(); //to load environment variables from .env file
 
 const app = express();
 const port = process.env.PORT || 5000; // Use the port from environment variables or default to 3000
+
+app.use(cors()); 
 
 app.use(express.json()); // Middleware to parse JSON request body
 app.use("/api/products", productRoute); // Mount the product route on /products
@@ -17,4 +20,4 @@ app.listen(port, () => {
   connectDB();
   console.log(`Example app listening on port ${port}`);
 });
- 
+  
